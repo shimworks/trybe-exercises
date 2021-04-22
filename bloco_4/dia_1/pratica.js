dddfb408b1afe8fb08bb4f4971e8b22a5791fbc6
Teika <shimworks.tk@gmail.com>
2021-04-22 18:35:04 -0300

    resolve 3 da pratica

---
+++ bloco_4/dia_1/pratica.js
@@ -0,0 +1,28 @@
+let a = 15;
+let b = 20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+
+let c = 90;
+let d = 60;
+let e = 80;
+
+let maior = c
+
+if (d > maior && d > e){
+    maior = d;
+}else if(e > maior && e > d){
+    maior = e;
+}
+console.log(maior)
+

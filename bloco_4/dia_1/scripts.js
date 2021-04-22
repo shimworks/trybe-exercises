6866898b4438745cfdee4205b625eec372ad890f
Teika <shimworks.tk@gmail.com>
2021-04-22 17:43:31 -0300

    resolve parte IV

--- bloco_4/dia_1/scripts.js
+++ bloco_4/dia_1/scripts.js
@@ -8,9 +8,19 @@
 // console.log(birthYear);
 
 //Parte III;
-var base = 5;
-var altura = 8;
-var area = base * altura;
-console.log(area);
-var perimetro = (base*2) + (altura*2);
-console.log(perimetro);
+// var base = 5;
+// var altura = 8;
+// var area = base * altura;
+// console.log(area);
+// var perimetro = (base*2) + (altura*2);
+// console.log(perimetro);
+
+//Parte IV
+let nota = 57;
+if (nota >= 80) {
+    console.log ("Parabéns, você foi aprovada(o)!");
+} else if (nota < 80 && nota >= 60){
+    console.log ("Você está na nossa lista de espera");
+} else {
+    console.log ("Você foi reprovada(o)");
+}

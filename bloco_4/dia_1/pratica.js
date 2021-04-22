209f50916aeb876c9bb3396db2abad4155c8d87a
Teika <shimworks.tk@gmail.com>
2021-04-22 19:21:44 -0300

    resolve parte 7

--- bloco_4/dia_1/pratica.js
+++ bloco_4/dia_1/pratica.js
@@ -85,3 +85,23 @@ switch (chessPiece.toLowerCase()) {
     console.log("Erro, peça inválida");
     break;
 }
+
+//Parte 7
+let porcentagem = 110
+
+if (porcentagem >= 90 && porcentagem <= 100){
+    console.log("A");
+} else if (porcentagem >= 80 && porcentagem < 90){
+    console.log("B");
+} else if (porcentagem >= 70 && porcentagem < 80){
+    console.log("C");
+} else if (porcentagem >= 60 && porcentagem < 70){
+    console.log("D");
+} else if (porcentagem >= 50 && porcentagem < 60){
+    console.log("E");
+} else if (porcentagem > 0 && porcentagem < 50){
+    console.log("F")
+} else {
+    console.log("Error");
+}
+

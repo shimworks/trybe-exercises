e991622293e741ef354bc33d336d4ae1e51a1ec9
Teika <shimworks.tk@gmail.com>
2021-04-22 19:12:48 -0300

    resolve 7

--- bloco_4/dia_1/pratica.js
+++ bloco_4/dia_1/pratica.js
@@ -54,3 +54,34 @@ if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
 } 
 
 //Parte 6
+let chessPiece = 'Peão';
+
+switch (chessPiece.toLowerCase()) {
+  case 'rei':
+    console.log("Rei: Uma casa apenas para qualquer direção.");
+    break;
+
+  case 'bispo':
+    console.log("Bispo: Diagonal.");
+    break;
+
+  case 'rainha':
+    console.log("Rainha: Diagonal, horizontal e vertical.");
+    break;
+
+  case 'cavalo':
+    console.log("Cavalo: L pode pular sobre as peças.");
+    break;
+
+  case 'torre':
+    console.log("Torre: Horizontal e vertical.");
+    break;
+
+  case 'peão':
+    console.log("Peão: Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+
+  default:
+    console.log("Erro, peça inválida");
+    break;
+}

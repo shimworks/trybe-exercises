6acf055b08f6d21ea63eb0368493209ae25044cc
Teika <shimworks.tk@gmail.com>
2021-04-22 15:55:56 -0300

    exercicios parte II e parte III

---
+++ bloco_4/dia_1/scripts.js
@@ -0,0 +1,16 @@
+// Parte II
+// const name = "Caio";
+// const birthCity = "Joaquim Távora";
+// var birthYear = 1997;
+// birthYear = 2030
+// console.log(name);
+// console.log(birthCity);
+// console.log(birthYear);
+
+//Parte III;
+var base = 5;
+var altura = 8;
+var area = base * altura;
+console.log(area);
+var perimetro = (base*2) + (altura*2);
+console.log(perimetro);

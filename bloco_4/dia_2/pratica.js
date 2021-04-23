673d5c19e5267e9cfc62ef4c350e7a98ed6f93fc
Teika <shimworks.tk@gmail.com>
2021-04-23 16:04:38 -0300

    resolve pratica 04

--- bloco_4/dia_2/pratica.js
+++ bloco_4/dia_2/pratica.js
@@ -21,7 +21,21 @@
 // soma = soma / numbers.length
 // console.log(soma);
 
-// Pratica 04
+// // Pratica 04
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0
+// for (let index = 0; index < numbers.length; index += 1){
+//     soma += numbers[index];
+// }
+// soma = soma / numbers.length
+// console.log(soma);
+// if (soma > 20){
+//     console.log("valor maior que 20");
+// } else{
+//     console.log("valor menor ou igual a 20");
+// }
+
+// Pratica 05
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0
 for (let index = 0; index < numbers.length; index += 1){
@@ -29,3 +43,8 @@ for (let index = 0; index < numbers.length; index += 1){
 }
 soma = soma / numbers.length
 console.log(soma);
+if (soma > 20){
+    console.log("valor maior que 20");
+} else{
+    console.log("valor menor ou igual a 20");
+}

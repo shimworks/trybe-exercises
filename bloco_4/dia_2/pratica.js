2479b3fcf47d5e4b8cdf85f933c4dc4d406d045d
Teika <shimworks.tk@gmail.com>
2021-04-23 16:00:49 -0300

    resolve pratica 01 a 03

---
+++ bloco_4/dia_2/pratica.js
@@ -0,0 +1,31 @@
+// // Pratica 01
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let i = 0; numbers.length > i; i += 1){
+//     console.log(numbers[i]);
+// }
+
+// // Pratica 02
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0
+// for (let index = 0; index < numbers.length; index += 1){
+//     soma += numbers[index];
+// }
+// console.log(soma);
+
+// // Pratica 03
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0
+// for (let index = 0; index < numbers.length; index += 1){
+//     soma += numbers[index];
+// }
+// soma = soma / numbers.length
+// console.log(soma);
+
+// Pratica 04
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0
+for (let index = 0; index < numbers.length; index += 1){
+    soma += numbers[index];
+}
+soma = soma / numbers.length
+console.log(soma);

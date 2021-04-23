478d36aa3d01d4ffb8a8661fa2c0d8f4d11ccfb6
Teika <shimworks.tk@gmail.com>
2021-04-23 16:10:27 -0300

    resolve pratica 5

--- bloco_4/dia_2/pratica.js
+++ bloco_4/dia_2/pratica.js
@@ -39,12 +39,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0
 for (let index = 0; index < numbers.length; index += 1){
-    soma += numbers[index];
+    if (soma < numbers[index]){
+        soma = numbers[index]
+    } 
 }
-soma = soma / numbers.length
-console.log(soma);
-if (soma > 20){
-    console.log("valor maior que 20");
-} else{
-    console.log("valor menor ou igual a 20");
-}
+console.log(soma)

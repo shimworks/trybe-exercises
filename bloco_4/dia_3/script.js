a929cb8e2dc9adda6e5c1b19869e2a333b132755
Teika <shimworks.tk@gmail.com>
2021-04-26 15:34:17 -0300

    resolve aprofunde 01

---
+++ bloco_4/dia_3/script.js
@@ -0,0 +1,10 @@
+// // Aprofunde 01
+// let num = 5;
+// let stars = "*";
+// let linha = "";
+// for (let index = 0; index < num; index += 1) {
+//     linha = linha + stars
+// };
+// for (let index = 0; index < num; index += 1) {
+//     console.log(linha);
+// };

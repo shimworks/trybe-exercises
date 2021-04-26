fe5d081bd4dad89d186073d3f01b51978946f3c3
Teika <shimworks.tk@gmail.com>
2021-04-26 17:55:05 -0300

    resolve aprofunde 03 (gabarito)

--- bloco_4/dia_3/script.js
+++ bloco_4/dia_3/script.js
@@ -17,3 +17,22 @@
 //     linha = linha + stars
 //     console.log(linha)
 // };
+
+// Aprofunde 03
+let num = 5;
+let stars = "*";
+let linha = "";
+let posicao = num;
+
+for (let indexLinha = 0; indexLinha < num; indexLinha += 1) {
+    for (let indexColuna = 1; indexColuna <= num; indexColuna += 1){
+        if (indexColuna < posicao){
+            linha = linha + ' ';
+        } else {
+            linha = linha + stars;
+        };
+    };
+    console.log(linha);
+    linha = "";
+    posicao -= 1;
+};

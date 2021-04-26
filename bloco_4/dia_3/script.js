fe002abf2bf779160a550305d1c14cd092592032
Teika <shimworks.tk@gmail.com>
2021-04-26 18:30:05 -0300

    resolve Aprofunde 04 (gabarito)

--- bloco_4/dia_3/script.js
+++ bloco_4/dia_3/script.js
@@ -19,20 +19,43 @@
 // };
 
 // Aprofunde 03
-let num = 5;
-let stars = "*";
-let linha = "";
-let posicao = num;
+// let num = 5;
+// let stars = "*";
+// let linha = "";
+// let posicao = num;
 
-for (let indexLinha = 0; indexLinha < num; indexLinha += 1) {
-    for (let indexColuna = 1; indexColuna <= num; indexColuna += 1){
-        if (indexColuna < posicao){
-            linha = linha + ' ';
-        } else {
-            linha = linha + stars;
-        };
-    };
-    console.log(linha);
-    linha = "";
-    posicao -= 1;
-};
+// for (let indexLinha = 0; indexLinha < num; indexLinha += 1) {
+//     for (let indexColuna = 1; indexColuna <= num; indexColuna += 1){
+//         if (indexColuna < posicao){
+//             linha = linha + ' ';
+//         } else {
+//             linha = linha + stars;
+//         };
+//     };
+//     console.log(linha);
+//     linha = "";
+//     posicao -= 1;
+// };
+
+// // Aprofunde 4
+// let num = 5;
+// let estrela = "*";
+// let linha = "";
+
+// let meio = (num + 1) / 2;
+// let esquerda = meio;
+// let direita = meio;
+
+// for (let indexLinha = 0; indexLinha <= meio; indexLinha += 1) {
+//     for (let indexColuna = 0; indexColuna <= num; indexColuna += 1) {
+//         if (indexColuna > esquerda && indexColuna < direita) {
+//             linha = linha + estrela;
+//         } else {
+//             linha = linha + " ";
+//         }
+//     }
+//     console.log(linha);
+//     linha = "";
+//     direita += 1;
+//     esquerda -= 1;
+// }

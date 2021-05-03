305a196633a19396b52072850079a4b1179c4079
Teika <shimworks.tk@gmail.com>
2021-05-03 18:31:32 -0300

    resolve exercicio 01

--- bloco_5/dia_3/Exercicios/script.js
+++ bloco_5/dia_3/Exercicios/script.js
@@ -15,12 +15,30 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
-let ulListaDays = document.getElementById('days')
+let ulListaDays = document.getElementById('days');
+let holidays = [24, 25, 31]
+
 function criaDiasNumericos() {
-  for (let key of dezDaysList) {
+  for (let dia of dezDaysList) {
     let liListaDiv = document.createElement('li');
-    liListaDiv.innerHTML = [key];
-    ulListaDays.appendChild(liListaDiv);
+      if (dia === 24 || dia === 31) {
+      liListaDiv.className = 'day holiday';
+      liListaDiv.innerHTML = dia;
+      ulListaDays.appendChild(liListaDiv);
+    } else if (dia === 4 || dia === 11 || dia === 18) {
+      liListaDiv.className = 'day friday';
+      liListaDiv.innerHTML = dia;
+      ulListaDays.appendChild(liListaDiv);
+    } else if (dia === 25) {
+      liListaDiv.className= 'day holiday friday';
+      liListaDiv.innerHTML = dia;
+      ulListaDays.appendChild(liListaDiv);
+    } else {
+      liListaDiv.className = 'day';
+      liListaDiv.innerHTML = dia;
+      ulListaDays.appendChild(liListaDiv);
+    };
   };
 };
 criaDiasNumericos();
+

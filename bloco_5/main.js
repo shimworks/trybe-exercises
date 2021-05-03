3307f3ea94bc8afc0898c0482f6b9cb5bc79d246
Teika <shimworks.tk@gmail.com>
2021-05-03 16:24:24 -0300

    reolve conteudo Parte II (parte I nao tem exercicios)

--- bloco_5/main.js
+++ bloco_5/main.js
@@ -7,17 +7,38 @@ const myWebpage = document.getElementById('mySpotrybefy');
 /*
  Copie esse arquivo e edite apenas ele;
  Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?*/
+divUm.addEventListener('click', adicionaClasse);
+divDois.addEventListener('click', adicionaClasse);
+divTres.addEventListener('click', adicionaClasse);
+function adicionaClasse(classe) {
+  let elementoSelecionado = document.querySelector('.tech');
+  elementoSelecionado.classList.remove('tech');
+  classe.target.classList.add('tech');
+}
+ /*Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+com a classe 'tech';*/
+input.addEventListener('input', adicionaClasse) // é possivel usar a mesma função.
+ /*Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
-
-Segue abaixo um exemplo do uso de event.target:
+1. Que tal redirecionar para seu portifólio?*/
+myWebpage.addEventListener('dblclick', doubleClick, 'mouseover', alteraCor);
+function doubleClick () {
+  window.open('https://github.com/shimworks/shimworks.github.io');
+}
+ /*Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+a cor do mesmo;*/
+myWebpage.addEventListener('mouseover', alteraCor)
+myWebpage.addEventListener('mouseleave', alteraCorDeVolta)
+function alteraCor(local) {
+  local.target.style.color = 'lightblue'
+}
+function alteraCorDeVolta(local) {
+  local.target.style.color = 'unset'
+}
+/*Segue abaixo um exemplo do uso de event.target:
 */
+
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.

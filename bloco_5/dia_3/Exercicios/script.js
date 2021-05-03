1e06d4dc42dcdf4d5f385a765f2669cab0e5c79a
Teika <shimworks.tk@gmail.com>
2021-05-03 17:19:41 -0300

    resolve parte do exercicio 1

--- bloco_5/dia_3/Exercicios/script.js
+++ bloco_5/dia_3/Exercicios/script.js
@@ -15,3 +15,12 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+let ulListaDays = document.getElementById('days')
+function criaDiasNumericos() {
+  for (let key of dezDaysList) {
+    let liListaDiv = document.createElement('li');
+    liListaDiv.innerHTML = [key];
+    ulListaDays.appendChild(liListaDiv);
+  };
+};
+criaDiasNumericos();

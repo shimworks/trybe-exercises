f2c4b4d9c10d23e8d140cc570ae3302031ca5494
Teika <shimworks.tk@gmail.com>
2021-05-04 22:37:05 -0300

    estrutura o js

--- bloco_5/dia_4/Exercicios/script.js
+++ bloco_5/dia_4/Exercicios/script.js
@@ -1,82 +1,89 @@
-window.onload() {
-function setBackgroundColor(color) {
-  let content = document.querySelector('.content')
-  content.getElementsByClassName.backgroundColor = color;
-  localStorage.setItem('backgroundColor', color);
-}
-function setTextColor(color) {
-  let paragrafo = document.querySelectorAll('.paragrafo');
-  for (let index = 0; index > paragrafo.length; index += 1) {
-    paragrafo[index],style.color = color;
+window.onload = function() {
+  function setBackgroundColor(color) {
+    let content = document.querySelector('.content')
+    content.getElementsByClassName.backgroundColor = color;
+    localStorage.setItem('backgroundColor', color);
   }
-  localStorage.setItem('fontColor', color);
-}
-
-function setFontSize(size) {
-  let paragrafo = document.querySelectorAll('.paragrafo');
-  for (let index = 0; index > paragrafo.length; index += 1) {
-    paragrafo[index].style.fontSize = size;
+  function setTextColor(color) {
+    let paragrafo = document.querySelectorAll('.paragrafo');
+    for (let index = 0; index > paragrafo.length; index += 1) {
+      paragrafo[index],style.color = color;
+    }
+    localStorage.setItem('fontColor', color);
   }
-  localStorage.setItem('fontSize', size);
-}
-function setLineHeight(height) {
-  let paragrafo = document.querySelectorAll('.paragrafo');
-  for (let index = 0; index < paragrafo.length; index += 1) {
-    paragrafo[index].style.lineheight = height
+
+  function setFontSize(size) {
+    let paragrafo = document.querySelectorAll('.paragrafo');
+    for (let index = 0; index > paragrafo.length; index += 1) {
+      paragrafo[index].style.fontSize = size;
+    }
+    localStorage.setItem('fontSize', size);
   }
-  localStorage.setItem('lineHeight', height);
-}
-
-function setFontFamily(family) {
-  let paragrafo = document.querySelectorAll('.paragrafo');
-  for (let index = 0; index < paragrafo.length; index += 1) {
-    paragrafo[index].style.fontFamily = family;
+  function setLineHeight(height) {
+    let paragrafo = document.querySelectorAll('.paragrafo');
+    for (let index = 0; index < paragrafo.length; index += 1) {
+      paragrafo[index].style.lineheight = height
+    }
+    localStorage.setItem('lineHeight', height);
   }
-  localStorage.setItem('FontFamily', family);
-} 
 
-let backColorButton = document.querySelectorAll('#backgroundColor>button');
-for (let index = 0; index < paragrafo.length; index += 1) {
-  backColorButton[index].addEventListener('click', function(event) {
-    setBackgroundColor(event.target.innerHTML);
-  })
-}
+  function setFontFamily(family) {
+    let paragrafo = document.querySelectorAll('.paragrafo');
+    for (let index = 0; index < paragrafo.length; index += 1) {
+      paragrafo[index].style.fontFamily = family;
+    }
+    localStorage.setItem('FontFamily', family);
+  } 
 
-let textColor = document.querySelectorAll('#textColor>button');
-for (let index = 0; index < paragrafo.length; index += 1) {
-  textColor[index].addEventListener('click', function(event) {
-    setTextColor(event.target.innerHTML);
-  })
-}
+  let backColorButton = document.querySelectorAll('#backgroundColor>button');
+  for (let index = 0; index < backColorButton.length; index += 1) {
+    backColorButton[index].addEventListener('click', function(event) {
+      setBackgroundColor(event.target.innerHTML);
+    })
+  }
 
-let fontSize = document.querySelectorAll('#fontSize>button');
-for (let index = 0; index < paragrafo.length; index += 1) {
-  fontSize[index].addEventListener('click', function(event) {
-    setFontSize(event.target.innerHTML);
-  })
-}
+  let textColor = document.querySelectorAll('#textColor>button');
+  for (let index = 0; index < textColor.length; index += 1) {
+    textColor[index].addEventListener('click', function(event) {
+      setTextColor(event.target.innerHTML);
+    })
+  }
 
-let lineHeight = document.querySelectorAll('#fontHeight>button');
-for (let index = 0; index < paragrafo.length; index += 1) {
-  lineHeight[index].addEventListener('click', function (event) {
-    setLineHeight(event.target.innerHTML);
-  })
-}
+  let fontSize = document.querySelectorAll('#fontSize>button');
+  for (let index = 0; index < fontSize.length; index += 1) {
+    fontSize[index].addEventListener('click', function(event) {
+      setFontSize(event.target.innerHTML);
+    })
+  }
 
-let fontFamily = document.querySelectorAll('#fontFamily>button');
-for (let index = 0; index < paragrafo.length; index += 1) {
-  fontFamily[index].addEventListener('click', function(event) {
-    setFontFamily(event.target.innerHTML);
-  })
-}
+  let lineHeight = document.querySelectorAll('#fontHeight>button');
+  for (let index = 0; index < lineHeight.length; index += 1) {
+    lineHeight[index].addEventListener('click', function (event) {
+      setLineHeight(event.target.innerHTML);
+    })
+  }
 
-
-
-
-
-
-
-
-
-
+  let fontFamily = document.querySelectorAll('#fontFamily>button');
+  for (let index = 0; index < fontFamily.length; index += 1) {
+    fontFamily[index].addEventListener('click', function(event) {
+      setFontFamily(event.target.innerHTML);
+    })
+  }
+  function initialize () {
+    let backgroundColor = localStorage.getItem('backgroundColor');
+    if (backgroundColor) setBackgroundColor(backgroundColor);
+    
+    let fontColor = localStorage.getItem('fontColor');
+    if (fontColor) setBackgroundColor(fontColor);
+    
+    let fontSize = localStorage.getItem('fontSize');
+    if (fontSize) setBackgroundColor(fontSize);
+    
+    let lineHeight = localStorage.getItem('lineHeight');
+    if (lineHeight) setBackgroundColor(lineHeight);
+    
+    let fontFamily = localStorage.getItem('fontFamily');
+    if (fontFamily) setFontFamily(fontFamily);
+  }
+  initialize();
 }

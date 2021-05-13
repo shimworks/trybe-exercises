2350479f4c2acfbcb852b66a14505599d10e44ba
Teika <shimworks.tk@gmail.com>
2021-05-12 22:06:04 -0300

    comeca estilizaçao via boostrap

--- bloco_6/dia_2/script.js
+++ bloco_6/dia_2/script.js
@@ -9,8 +9,26 @@ function optionCreator() {
 }
 optionCreator();
 
-var picker = new Pikaday({ field: document.getElementById('datepicker') });
+let picker = new Pikaday({ field: document.getElementById('datepicker') });
+(function () {
+  'use strict'
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
 function dateValidation(input, name) {
   if (input.value.length === 0) {

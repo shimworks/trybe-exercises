17e0e9e439a657807e6c3ab10ae344640c454244
Teika <shimworks.tk@gmail.com>
2021-05-27 18:39:13 -0300

    resolve exercicio 7

--- bloco_8/dia_3/Exercies/exercice7.js
+++ bloco_8/dia_3/Exercies/exercice7.js
@@ -63,4 +63,9 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+const expectedResult = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  return books.find((element) => (element.author.name.split(' ').filter((word) => word.endsWith('.')).length === 3)).name;
+}
+assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);

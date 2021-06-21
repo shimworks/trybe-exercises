670dde50f6376a2116ae9884aefc99f0204ae063
Teika <shimworks.tk@gmail.com>
2021-06-21 15:20:13 -0300

    testes

--- bloco_10/dia_3/Exercicios/exercicio.js
+++ bloco_10/dia_3/Exercicios/exercicio.js
@@ -3,14 +3,28 @@ const fetch = require ('node-fetch')
 function rng() {
   return Math.random()*100
 }
+function somaAR(call) {
+  const rdm = call();
+  return rdm * 5
+}
 
 async function funBeg() {
   let result = await fetch('https://dog.ceo/api/breeds/image/random')
   result = await result.json()
 }
 
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  return fetch(API_URL, { headers: { Accept: 'application/json' }})
+    .then(response => response.json())
+    .then(data => data.joke);
+};
+
 module.exports = {
   rng,
   funBeg,
+  fetchJoke,
+  somaAR,
 };
 
--- bloco_10/dia_3/Exercicios/exercicio.test.js
+++ bloco_10/dia_3/Exercicios/exercicio.test.js
@@ -1,48 +1,66 @@
-const func = require('./exercicio');
-jest.mock('./exercicio')
+jest.mock("./exercicio")
+let { rng, somaAR, fetchJoke, funBeg} = require('./exercicio');
+const { expect } = require("@jest/globals");
 
 describe('testa rng', () => {
   it('rng', () => {
-    func.rng = jest.fn()
-      .mockReturnValue(10)
-
-      expect(func.rng()).toBe(10)
-      expect(func.rng).toHaveBeenCalled();
-      expect(func.rng).toHaveBeenCalledTimes(1)
+    rng = jest.fn()
+    .mockReturnValue(10)
+    
+    expect(rng()).toBe(10);
+    expect(somaAR(rng)).toBe(50)
   })
-  it('rng2', () => {
-    func.rng.mockReset();
-    func.rng.mockImplementation((a, b) => a / b)
+})
 
-      expect(func.rng(10, 2)).toBe(5);
-      expect(func.rng).toHaveBeenCalled();
-      expect(func.rng).toHaveBeenCalledTimes(1);
-      expect(func.rng).toHaveBeenCalledWith(10, 2);
+describe('testa rng', () => {
+  it('rng2', () => {
+    rng.mockReset();
+    rng.mockImplementation((a, b) => a / b)
+
+      expect(rng(10, 2)).toBe(5);
+      expect(rng).toHaveBeenCalled();
+      expect(rng).toHaveBeenCalledTimes(1);
+      expect(rng).toHaveBeenCalledWith(10, 2);
   })
   it('rng3', () => {
-    func.rng.mockReset();
-    func.rng.mockImplementation((a, b, c) => a * b * c)
-      expect(func.rng(2, 5, 2)).toBe(20);
-      expect(func.rng).toHaveBeenCalled();
-      expect(func.rng).toHaveBeenCalledTimes(1);
-      expect(func.rng).toHaveBeenCalledWith(2, 5, 2);
+    rng.mockReset();
+    rng.mockImplementation((a, b, c) => a * b * c)
+      expect(rng(2, 5, 2)).toBe(20);
+      expect(rng).toHaveBeenCalled();
+      expect(rng).toHaveBeenCalledTimes(1);
+      expect(rng).toHaveBeenCalledWith(2, 5, 2);
       
-    func.rng.mockReset();
-    func.rng.mockImplementation((a) => a * 2);
-      expect(func.rng(5)).toBe(10);
-      expect(func.rng).toHaveBeenCalled();
-      expect(func.rng).toHaveBeenCalledTimes(1);
-      expect(func.rng).toHaveBeenCalledWith(5);
+    rng.mockReset();
+    rng.mockImplementation((a) => a * 2);
+      expect(rng(5)).toBe(10);
+      expect(rng).toHaveBeenCalled();
+      expect(rng).toHaveBeenCalledTimes(1);
+      expect(rng).toHaveBeenCalledWith(5);
   })
 })
 
 describe('testa exercicio 6', () => {
   it('exec 6', async () => {
-    func.funBeg.mockReset();
-    func.funBeg.mockResolvedValueOnce('veio um dog feio')
-    func.funBeg.mockRejectedValueOnce('morreu na rua')
+    funBeg = jest.fn()
+    funBeg.mockReset();
+    funBeg.mockResolvedValueOnce('veio um dog feio')
+    funBeg.mockRejectedValueOnce('morreu no caminho pro seu PC')
 
-      await expect(func.funBeg()).resolves.toBe('veio um dog feio')
-      await expect(func.funBeg()).rejects.toBe('morreu na rua')
+      await expect(funBeg()).resolves.toBe('veio um dog feio')
+      await expect(funBeg()).rejects.toBe('morreu no caminho pro seu PC')
   })
+})
+
+describe('testa Bonus', () => {
+  it('fetch', async () => {
+    const obj = {
+      'id': '7h3oGtrOfxc',
+      'joke': 'Whiteboards ... are remarkable.',
+      'status': 200
+    }
+    fetchJoke = jest.fn().mockResolvedValueOnce(obj)
+    
+    await expect(fetchJoke()).resolves.toEqual(obj)
+    expect.assertions(1);
+  });
 })

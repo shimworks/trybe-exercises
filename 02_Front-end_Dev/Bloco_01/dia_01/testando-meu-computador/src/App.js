72f79383efd2a3786cf63ee6ddd6d73ac58673a7
Teika <shimworks.tk@gmail.com>
2021-06-21 18:33:19 -0300

    resolve exercicios

--- 02_Front-end_Dev/Bloco_01/dia_01/testando-meu-computador/src/App.js
+++ 02_Front-end_Dev/Bloco_01/dia_01/testando-meu-computador/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+const lista = ['Tarefa1', 'Tarefa2', 'Lavar louça']
+
+// const Task = (value) => {
+//   return (
+//     <li>{value}</li>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload 'ok'.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ol>
+          {lista.map((elm) => <li>{elm}</li>)}
+        </ol>
       </header>
     </div>
   );

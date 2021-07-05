b5103cbb426502ac2bbf78f63e6505eb8182a9ff
Teika <shimworks.tk@gmail.com>
2021-07-05 20:30:53 -0300

    resolve todos os exercicios

---
+++ 02_Front-end_Dev/Bloco_13/dia_2/exercise-pokedex-router-master/src/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class About extends React.Component {
+
+  render() { 
+    return (
+      <>
+      <h4>Se tu quiser saber o que é pokedex entre neste link</h4>
+      <a target="_blank" href="https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9dex">link</a>
+      </>
+    );
+  }
+}
+ 
+export default About;
--- 02_Front-end_Dev/Bloco_13/dia_2/exercise-pokedex-router-master/src/App.js
+++ 02_Front-end_Dev/Bloco_13/dia_2/exercise-pokedex-router-master/src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import PokeInfo from './PokeInfo'
 import pokemons from './data';
 import Pokedex from './Pokedex';
-import { BrowserRouter, Route } from 'react-router-dom'
+import About from './About';
+import PageNotFound from './PageNotFound'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
+    <Link to="/">Home</Link>
+    <br/>
+    <Link to="/about">About</Link>
     <div className="App">
       <h1> Pokedex </h1>
-    <Route exact path="/" render={(props) => <Pokedex {...props} pokemons={pokemons}/>}/>
-    <Route path="/pokemons/:id" render={(props) => <PokeInfo {...props} pokemons={pokemons}/>}/>
+      <Switch>
+        <Route exact path="/" render={(props) => <Pokedex {...props} pokemons={pokemons}/>}/>
+        <Route path="/pokemons/:id" render={(props) => <PokeInfo {...props} pokemons={pokemons}/>}/>
+        <Route path="/about" component={About}/>
+        <Route path="/" component={PageNotFound}/>
+      </Switch>
     </div>
     </BrowserRouter>
   );

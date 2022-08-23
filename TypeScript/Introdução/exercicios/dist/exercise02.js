1fdad74ab41927bedd7997091745df542cbf382a
Teika <shimworks.tk@gmail.com>
2022-08-23 16:46:06 -0300

    resolve exercicios 1 a 4

---
+++ TypeScript/Introdução/exercicios/dist/exercise02.js
@@ -0,0 +1,17 @@
+"use strict";
+var Months;
+(function (Months) {
+    Months[Months["Jan"] = 1] = "Jan";
+    Months[Months["Feb"] = 2] = "Feb";
+    Months[Months["Mar"] = 3] = "Mar";
+    Months[Months["Apr"] = 4] = "Apr";
+    Months[Months["May"] = 5] = "May";
+    Months[Months["Jun"] = 6] = "Jun";
+    Months[Months["Jul"] = 7] = "Jul";
+    Months[Months["Aug"] = 8] = "Aug";
+    Months[Months["Set"] = 9] = "Set";
+    Months[Months["Out"] = 10] = "Out";
+    Months[Months["Nov"] = 11] = "Nov";
+    Months[Months["Dec"] = 12] = "Dec";
+})(Months || (Months = {}));
+console.log(Months);
